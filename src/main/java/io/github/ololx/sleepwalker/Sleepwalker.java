fc7ab05cc30704c616144835e48f466da89f6d63
ololx <ololx@icloud.com>
2022-11-08 19:30:03 +0500

    chore(*) init project

---
+++ src/main/java/io/github/ololx/sleepwalker/Sleepwalker.java
@@ -0,0 +1,14 @@
+package io.github.ololx.sleepwalker;
+
+/**
+ * project sleepwalker
+ * created 08.11.2022 19:28
+ *
+ * @author Alexander A. Kropotin
+ */
+public class Sleepwalker {
+
+    public static void main(String[] args) {
+
+    }
+}
